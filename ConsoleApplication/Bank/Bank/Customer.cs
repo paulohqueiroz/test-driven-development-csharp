6a198711c4200af105b9a84ce14e2c7e991a274e
Paulo Queiroz <phqs.phqs@gmail.com>
2020-03-22 21:52:58 -0300

    add new tests for console application

---
+++ ConsoleApplication/Bank/Bank/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    class Customer
+    {
+        private readonly int _ID;
+        private readonly string _Name;
+        private string _Address;
+        private string _Category;
+        private bool _Status;
+
+        public Customer() { }
+        
+        public Customer(
+            string name,
+            string address, 
+            string category,
+            bool status
+        )
+        {
+            _Name = name;
+            _Address = address;
+            _Category = category;
+            _Status = status;
+        }
+
+        public string Name { get { return Name; } }
+        public string Address { get { return _Address; } }
+        public string Category { get { return _Category; } }
+        public bool Status { get { return _Status; } }
+
+
+    }
+}
